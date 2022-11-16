a5581dc59667e6ebcb2040913951f51e21655297
greewank <gkc1s@semo.edu>
2022-11-16 11:34:03 -0500

    Frontend

---
+++ src/actions/posts.js
@@ -0,0 +1,50 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from "../constants/actionTypes";
+import * as api from "../api";
+
+// Action creators (functions that return actions i.e the state and action from reducer). actions is an object that has a type and a payload.
+
+export const getPosts = () => async (dispatch) => {
+  try {
+    const { data } = await api.fetchPosts();
+    dispatch({ type: FETCH_ALL, payload: data });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+export const createPost = (post) => async (dispatch) => {
+  try {
+    const { data } = await api.createPost(post);
+    dispatch({ type: CREATE, payload: data });
+    console.log(data);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+export const updatePost = (id, post) => async (dispatch) => {
+  try {
+    const { data } = await api.updatePost(id, post);
+    dispatch({ type: UPDATE, payload: data });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id);
+    dispatch({ type: DELETE, payload: id });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const likePost = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.likePost(id);
+    dispatch({ type: UPDATE, payload: data });
+  } catch (err) {
+    console.log(err);
+  }
+};
